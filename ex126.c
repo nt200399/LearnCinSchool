f74944f6a96a65df73274c300376f2ff254bb0da
Thanh Nguyen <34387636+nt200399@users.noreply.github.com>
2018-04-22 10:38:30 +0700

    Add files via upload

--- ex126.c
+++ ex126.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-void tangluong(int luonghientai, int *luongmoi);
+void tangluong(int namcongtac, int luonghientai, int *luongmoi);
 
 int main()
 {
     int namcongtac, luonghientai, luongmoi;
+    tangluong(namcongtac, luonghientai, &luongmoi);
+    printf("Luong moi cua nhan vien la: %d\n", luongmoi);
+}
 
+void tangluong(int namcongtac, int luonghientai, int *luongmoi)
+{
     do
     {
         printf("Nhap so nam cong tac: ");
@@ -17,12 +22,6 @@ int main()
     printf("Nhap so luong hien tai: ");
     scanf("%d", &luonghientai);
 
-    tangluong(luonghientai, &luongmoi);
-    printf("Luong moi cua nhan vien la: %d\n", luongmoi);
-}
-
-void tangluong(int luonghientai, int *luongmoi)
-{
     int tientang;
     printf("Nhap so tien tang them: ");
     scanf("%d", &tientang);
