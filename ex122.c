48150cf24aa660f80b042dfb0fbfc9d4552bc20e
Thanh Nguyen <34387636+nt200399@users.noreply.github.com>
2018-04-23 10:19:36 +0700

    Update ex122.c

--- ex122.c
+++ ex122.c
@@ -4,14 +4,13 @@ double *giatrimax(double *arr, int n);
 
 int main()
 {
-    double arr[100];
+    double *arr;
     int n;
     printf("Nhap so phan tu: ");
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
-        scanf("%lf", &arr[i]);
-    if (giatrimax(arr, n) != NULL)
-        printf("Con tro tro toi gia tri lon nhat cua mang la: %p\n", giatrimax(arr, n));
+        scanf("%lf", &*(arr + i));
+    printf("Con tro tro toi gia tri lon nhat cua mang la: %.2lf\n", *giatrimax(arr, n));
     return 0;
 }
 
@@ -23,7 +22,7 @@ double *giatrimax(double *arr, int n)
     for (int i = 0; i < n; i++)
     {
         if (*max < *(arr + i))
-            *max < *(arr + i);
+            *max = *(arr + i);
     }
     return max;
 }
